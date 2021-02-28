6cbf9aca2b10c419724ffc685f3254c9635345e2
winfrom within <antonneturner@gmail.com>
2021-02-27 20:13:12 -0500

    add burger img, change css background, update css

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-  $(".change-burger").on("click", function (event) {
+  $(".change-burgers").on("click", function (event) {
     var id = $(this).data("id");
 
     var newBurgerState = {
